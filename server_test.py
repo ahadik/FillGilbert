58b34a762ab22aaf101239fbe4ebef51b85f2a1b
ahadik <ahadik@us.ibm.com>
2015-12-02 21:05:46 -0600

    changed Procfile to test server

---
+++ server_test.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request, url_for
+from Tweeter import *
+#import threading
+
+key = '4qwcMrkw08dZRF8JOUAbTPWEI'
+secret = 'GnmdzLDjbIwruXveJ4RJePeAm5W2MP7a4jxXZbpNrUh46NAoRf'
+screen_name = 'philgilbertsr'
+tweets = TweetCorpus(key, secret, screen_name)
+
+app = Flask(__name__, template_folder="./templates/", static_folder="./static")
+
+#tweet_thread = threading.Thread(target=tweets.compile)
+#tweet_thread.start()
+
+@app.route("/")
+def index():
+	return "Hello World"
+
+'''
+@app.route("/slack", methods=["POST"])
+def slack():
+	if tweets.compiled:
+		if request.form['command'] == '/fill_gilbert':
+			value = None
+			try:
+				value = int(request.form['text'])
+			except ValueError:
+				return 'Give Phil a number of paragraphs to say.'
+			return tweets.compose(value)
+	else:
+		return 'Compiling Phil\'s thoughts just a second...'
+'''
+
+
+if __name__ == "__main__":
+	app.run()
